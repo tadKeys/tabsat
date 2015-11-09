1d359db3139626de76d6350d0939f95fe4021a27
Stephan Pabinger <stephan.pabinger@gmail.com>
2015-11-09 17:47:52 +0100

    Improved test case

--- tools/zz_test/test_tabsat.sh
+++ tools/zz_test/test_tabsat.sh
@@ -5,15 +5,16 @@ echo -e "\nRunning Tabsat test \n"
 USER_HOME=$HOME
 BASE_DIR="${USER_HOME}/tabsat"
 TABSAT="${BASE_DIR}/tabsat"
-TARGET_FILE="${BASE_DIR}/zz_test/target_list.csv"
 
+TEST_DIR="${BASE_DIR}/tools/zz_test"
+TARGET_FILE="${TEST_DIR}/target_list.csv"
 TEST_OUTPUT="${BASE_DIR}/tabsat_test_output"
 
 ## Create directory
 mkdir -p ${TEST_OUTPUT}
 
 ## Test with file list
-FILE1="${BASE_DIR}/zz_test/PGM_316D_IonXpress_032_1.fastq"
+FILE1="${TEST_DIR}/PGM_316D_IonXpress_028_1.fastq"
 
 ${TABSAT} -l NONDIR -q 21 -m 9 -p 0.7 -r 4 -t "${TARGET_FILE}" -a tmap -o ${TEST_OUTPUT} ${FILE1}
 
