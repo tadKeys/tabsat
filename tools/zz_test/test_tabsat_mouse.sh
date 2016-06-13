b0cf8ca148611ae79fdbbaff294bbea2ba244a24
Stephan Pabinger <stephan.pabinger@gmail.com>
2016-06-13 15:40:30 +0200

    Updated test cases

--- tools/zz_test/test_tabsat_mouse.sh
+++ tools/zz_test/test_tabsat_mouse.sh
@@ -7,15 +7,17 @@ BASE_DIR="${USER_HOME}/tabsat"
 TABSAT="${BASE_DIR}/tabsat"
 
 TEST_DIR="${BASE_DIR}/tools/zz_test"
-TARGET_FILE="${TEST_DIR}/target_list.csv"
+TARGET_FILE="${TEST_DIR}/target_list_mouse.csv"
 TEST_OUTPUT="${BASE_DIR}/tabsat_test_output_mouse"
 
 ## Create directory
 mkdir -p ${TEST_OUTPUT}
 
 ## Test with file list
-FILE1="${TEST_DIR}/PGM_316D_IonXpress_028_1.fastq"
+FILE1="${TEST_DIR}/SRR2734636_head_1.fastq"
+FILE2="${TEST_DIR}/SRR2734636_head_2.fastq"
 
-${TABSAT} -l NONDIR -q 21 -m 9 -p 0.7 -r 4 -t "${TARGET_FILE}" -a tmap -o ${TEST_OUTPUT} ${FILE1}
+
+${TABSAT} -e PE -g mm10 -l NONDIR -q 21 -m 9 -p 0.7 -r 10 -t "${TARGET_FILE}" -a bowtie2 -o ${TEST_OUTPUT} ${FILE1} ${FILE2}
 
 
--- tools/zz_test/test_tabsat_tmap.sh
+++ tools/zz_test/test_tabsat_tmap.sh
@@ -7,8 +7,8 @@ BASE_DIR="${USER_HOME}/tabsat"
 TABSAT="${BASE_DIR}/tabsat"
 
 TEST_DIR="${BASE_DIR}/tools/zz_test"
-TARGET_FILE="${TEST_DIR}/target_list.csv"
-TEST_OUTPUT="${BASE_DIR}/tabsat_test_output"
+TARGET_FILE="${TEST_DIR}/target_list_tmap.csv"
+TEST_OUTPUT="${BASE_DIR}/tabsat_test_output_tmap"
 
 ## Create directory
 mkdir -p ${TEST_OUTPUT}
@@ -16,6 +16,6 @@ mkdir -p ${TEST_OUTPUT}
 ## Test with file list
 FILE1="${TEST_DIR}/PGM_316D_IonXpress_028_1.fastq"
 
-${TABSAT} -l NONDIR -q 21 -m 9 -p 0.7 -r 4 -t "${TARGET_FILE}" -a tmap -o ${TEST_OUTPUT} ${FILE1}
+${TABSAT} -l NONDIR -e SE -g hg19 -q 21 -m 9 -p 0.7 -r 4 -t "${TARGET_FILE}" -a tmap -o ${TEST_OUTPUT} ${FILE1}
 
 
