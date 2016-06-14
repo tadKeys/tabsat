ff8394487b4d07afe38e831a08f89756a5ab30d7
Stephan Pabinger <stephan.pabinger@gmail.com>
2016-06-14 15:01:58 +0200

    Comparison of bowtie vs tmap

--- comp_mapping/check_mapping_tmap_bowtie.py
+++ comp_mapping/check_mapping_tmap_bowtie.py
@@ -3,21 +3,24 @@ import sys
 import glob
 import subprocess
 import os
+import numpy
 
 
-## 
+## Based on the deep coverage of up to 3300 1-2% seems reasonable for the cutoff
+min_read_depth = 45
 
-def create_section(section_start, prev_end, interval, cur_sum):
+
+def create_section(section_start, prev_end, section_chrom, cur_sum):
 
     if cur_sum == 0:
         print section_start
-        print interval
+        print section_chrom
         cur_sum = 1
 
     #print cur_sum
     #print (int(interval.start) - int(section_start))
 
-    return str(interval.chrom) + "\t" + str(section_start) + "\t" + str(prev_end) + str("\t") + str( float(cur_sum) / (float(prev_end) - float(section_start)) )  ## don't us end as we are already in the next section
+    return str(section_chrom) + "\t" + str(section_start) + "\t" + str(prev_end) + str("\t") + str( float(cur_sum) / (float(prev_end) - float(section_start)) )  ## don't us end as we are already in the next section
 
 
 def print_off_targets(off_target_sections):
@@ -57,12 +60,12 @@ def summary_off_target_mapping(bed_file_path, sam_file_path):
 
 	
 
-        if int(interval.count) < 25:
+        if int(interval.count) <= min_read_depth:
 
             ## Check if in the middle of a section
             if cur_sum > 0:
                 #print "printint first"
-                off_target_sections.append(create_section(section_start, prev_end, interval, cur_sum))
+                off_target_sections.append(create_section(section_start, prev_end, section_chrom, cur_sum))
                 section_start = 0
 		section_chrom = None
                 cur_sum = 0
@@ -80,7 +83,7 @@ def summary_off_target_mapping(bed_file_path, sam_file_path):
             if section_start > 0:
                 ## Print the interval
                 #print "printing second"
-                off_target_sections.append(create_section(section_start, prev_end, interval, cur_sum))
+                off_target_sections.append(create_section(section_start, prev_end, section_chrom, cur_sum))
 
             section_start = int(interval.start)
 	    section_chrom = interval.chrom
@@ -173,9 +176,11 @@ def main():
 
 
     print "TMAP\n"
-    tmap_off_targets = list()
+    tmap_off_targets = dict()
     for f in tmap_sam_files:
-        tmap_off_targets.append(get_stats_for_file(target_file, f))
+	f_name = os.path.basename(f).split(".")[0]
+        tmap_off_targets[f_name] = get_stats_for_file(target_file, f)
+#	break
 
     tmap_total_ot = 0
     for t in tmap_off_targets:
@@ -187,9 +192,11 @@ def main():
 
     print "\nBOWTIE\n"
 
-    bowtie_off_targets = list()
+    bowtie_off_targets = dict()
     for f in bowtie_sam_files:
-        bowtie_off_targets.append(get_stats_for_file(target_file, f))
+	f_name = os.path.basename(f).split(".")[0]
+        bowtie_off_targets[f_name] = get_stats_for_file(target_file, f)
+#	break
 
     bowtie_total_ot = 0
     for t in bowtie_off_targets:
@@ -198,6 +205,59 @@ def main():
     print "Total: " + str(bowtie_total_ot)
 
 
+
+    ## Intersect
+    print "Intersect"
+
+    #print tmap_off_targets
+    #print bowtie_off_targets
+
+
+    list_num_not_overlapping = list()
+
+    for t_file in sorted(tmap_off_targets):
+
+	t_t = tmap_off_targets[t_file]
+	b_t = bowtie_off_targets[t_file]
+#	b_t = bowtie_off_targets.values()[0]
+
+	print "File: " + str(t_file)
+	print "Number off-targets tmap: " + str(len(t_t))
+	print "Number off-targets bowtie: " + str(len(b_t))
+
+	## Create bed files
+	t_bed = pybedtools.BedTool("\n".join(t_t), from_string=True)
+	b_bed = pybedtools.BedTool("\n".join(b_t), from_string=True)
+	#print "a: " + str(t_bed)
+	#print "b: " + str(b_bed)
+
+
+	#print "Closest: "
+	closest = t_bed.closest(b_bed, d=True)
+	#print closest
+	#print len(closest)
+
+	num_not_overlapping = 0
+
+	for c in closest:
+	    #print c
+	    distance = c[8]
+	    #print c[8]
+	    if not int(distance) == 0:
+		#print distance
+		num_not_overlapping += 1
+
+	print "# not overlapping: " + str(num_not_overlapping)
+
+	list_num_not_overlapping.append(num_not_overlapping)
+
+    print ""
+    print "Median not overlapping: " + str(numpy.median(list_num_not_overlapping))
+    print "Mean not overlapping: " + str(numpy.mean(list_num_not_overlapping))
+    
+
+
+	
 
 
 
